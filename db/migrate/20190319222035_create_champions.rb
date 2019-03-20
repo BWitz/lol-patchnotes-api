f1cf8e994f708bd277aa3fd6fff60c5d74cd601d
Bwitz <guitarrook94@gmail.com>
2019-03-20 00:03:52 -0400

    Added image sources to the database schema

--- db/migrate/20190319222035_create_champions.rb
+++ db/migrate/20190319222035_create_champions.rb
@@ -2,15 +2,15 @@ class CreateChampions < ActiveRecord::Migration[5.2]
   def change
     create_table :champions do |t|
       t.string :name
-      t.string :developer_commentary
-      t.string :img_src1
-      t.string :img_src2
+      t.text :developer_commentary
+      t.string :icon_img_src
+      t.string :splashart_img_src
       t.string :ability1
       t.string :ability2
       t.string :ability3
       t.string :ability4
       t.string :ability5
-      t.string :change_summary
+      t.text :change_summary
       t.integer :last_patch_update
 
       t.timestamps
