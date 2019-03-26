ba859035402a14a23e8d5bcf40f091e27a204953
Bwitz <guitarrook94@gmail.com>
2019-03-25 22:38:54 -0400

    Resolved issues with the champion items join table.

--- app/models/champion_item.rb
+++ app/models/champion_item.rb
@@ -1,4 +1,4 @@
 class ChampionItem < ApplicationRecord
-    belongs_to :champion, class_name: "champion", foreign_key: "champion_id"
-    belongs_to :item, class_name: "item", foreign_key: "item_id"
+    belongs_to :champion, class_name: "Champion", foreign_key: "champion_id"
+    belongs_to :item, class_name: "Item", foreign_key: "item_id"
 end
